0174cfdee799df631e1d039073149bc421211fc3
mdanieldantas <contatomarcosdgomes@gmail.com>
2025-01-21 17:08:18 -0300

    adiciona rota para salvar uma nova lista de tarefas

--- src/router.js
+++ src/router.js
@@ -6,10 +6,14 @@ const router = express.Router();
 
 //rota principal da aplicação home
 router.get("/",(req,res)=>{res.render("index");});
+
 // rota da lista de tarefas
 router.get("/app", taskLisListController.index);
 // rota para criar uma nova lista de tarefas
 router.get("/app/nova-lista", taskLisListController.create);
+// rota para salvar uma nova lista de tarefas
+router.post("/app/nova-lista", taskLisListController.save);
+
 
 
 module.exports = router;
