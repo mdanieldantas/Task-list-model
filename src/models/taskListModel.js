ee9baba10405badcc54631c4c50a4897960f5f1e
mdanieldantas <contatomarcosdgomes@gmail.com>
2025-01-21 17:08:36 -0300

    adiciona função para criar e salvar novas listas de tarefas

--- src/models/taskListModel.js
+++ src/models/taskListModel.js
@@ -1,14 +1,27 @@
 let taskLists = [
-    { id: '1', title: 'Estudos', tasks: [{ id: '1', title: 'Estudar Node', completed: false }] },
-    { id: '2', title: 'Tarefas de casa', tasks: [] },
-    { id: '3', title: 'Coisas do trabalho', tasks: [] }
-  ]
-
-  module.exports = {
-
-    getAllTaskLists: (req, res) => {
-        return taskLists
+    {
+      id: "1",
+      title: "Estudos",
+      tasks: [{ id: "1", title: "Estudar Node", completed: false }],
     },
-
-
+    { id: "2", title: "Tarefas de casa", tasks: [] },
+    { id: "3", title: "Coisas do trabalho", tasks: [] },
+  ];
+  
+  module.exports = {
+    getAllTaskLists: (req, res) => {
+      return taskLists;
+    },
+  
+    createTaskList: (title) => {
+      const newList = {
+        id: Math.floor(Math.random() * 999999999).toString(),
+        title: title,
+      };
+      return newList;
+    },
+    saveList: (taskList) => {
+      if (taskList.title === "") throw new Error("O titulo nao pode ser vazio");
+      taskLists.push(taskList);
+    },
   };
