10849c013c8c1384379f4bfebebd352a21315d06
mdanieldantas <contatomarcosdgomes@gmail.com>
2025-01-21 16:19:13 -0300

    adiciona rotas para a aplicação e controlador de listas de tarefas

--- src/router.js
+++ src/router.js
@@ -1,5 +1,15 @@
+
 const express = require('express');
+const taskLisListController = require('./controllers/taskListController');
 
 const router = express.Router();
+
+//rota principal da aplicação home
+router.get("/",(req,res)=>{res.render("index");});
+// rota da lista de tarefas
+router.get("/app", taskLisListController.index);
+// rota para criar uma nova lista de tarefas
+router.get("/app/nova-lista", taskLisListController.create);
+
 
 module.exports = router;
