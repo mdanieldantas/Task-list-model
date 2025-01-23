1949ea6b40cf30251a1997577c234d07ca4982a9
mdanieldantas <contatomarcosdgomes@gmail.com>
2025-01-23 12:49:36 -0300

    adiciona rota para excluir tarefas de listas

--- src/router.js
+++ src/router.js
@@ -11,11 +11,14 @@ router.get("/",(req,res)=>{res.render("index");});
 router.get("/app", taskLisListController.index);
 router.get("/app/nova-lista", taskLisListController.create);
 router.post("/app/nova-lista", taskLisListController.save);
+
 router.get("/app/:id", taskLisListController.show);
 router.post("/app/:id/nova-tarefa", taskLisListController.addTask);
+router.post("/app/:id/excluir", taskLisListController.delete);
+
 router.post("/app/:listId/completar/:taskId", taskLisListController.completeTask);
 router.post("/app/:listId/desfazer/:taskId", taskLisListController.undoTask);
-router.post("/app/:id/excluir", taskLisListController.delete);
+
 
 
 
