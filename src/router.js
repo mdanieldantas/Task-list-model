2c24f66b07cfa2b9ca57e7102051836b0163d84d
mdanieldantas <contatomarcosdgomes@gmail.com>
2025-01-23 11:44:01 -0300

    adiciona rotas para completar, desfazer e excluir tarefas

--- src/router.js
+++ src/router.js
@@ -13,8 +13,9 @@ router.get("/app/nova-lista", taskLisListController.create);
 router.post("/app/nova-lista", taskLisListController.save);
 router.get("/app/:id", taskLisListController.show);
 router.post("/app/:id/nova-tarefa", taskLisListController.addTask);
-
-
+router.post("/app/:listId/completar/:taskId", taskLisListController.completeTask);
+router.post("/app/:listId/desfazer/:taskId", taskLisListController.undoTask);
+router.post("/app/:id/excluir", taskLisListController.delete);
 
 
 
